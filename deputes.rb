919631ab784e0efa42c9d65a1c66e0791aa41dfd
Snakultime <snakultime@hotmail.fr>
2018-10-12 00:18:20 +0200

    deputes.rb v2.0

--- deputes.rb
+++ deputes.rb
@@ -2,18 +2,17 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 
-def lastname_firstname
-    @name = []
+@name = []
+@email = []
+
+def name_prename
     doc = Nokogiri::HTML(open("http://www2.assemblee-nationale.fr/deputes/liste/alphabetique"))
     doc.xpath('/html/body/div/div/div/section/div/article/div/div/div/div/ul/li/a').each do |i|
     @name.push({ Prénom: i.text.split(' ')[1], Nom: i.text.split(' ')[2]})
   end
-  puts @name
 end
 
-
 def email_value
-    @email = []
     doc = Nokogiri::HTML(open("https://www.voxpublic.org/spip.php?page=annuaire&cat=deputes&pagnum=576"))
     doc.css('a[@href ^="mailto:"]').each do |e|
     @email << e.text
@@ -21,9 +20,22 @@ def email_value
   @email.keep_if { |w| w =~ /assemblee-nationale.fr$/ }
   @email.delete_if {|w| w =~ /secretariat-blanchet/}
   @email.delete_if {|w| w =~ /bureau-m-orphelin/}
-
-  puts @email
 end
 
-lastname_firstname
-email_value
+def full_name
+  return @name
+end
+
+def emails
+  return @email
+end
+
+def perform
+  name_prename
+  email_value
+  full_name.length.times do |i|
+    puts "Prénom: " + full_name[i][:Prénom] + " - Nom: " + full_name[i][:Nom] + " - email: " + emails[i - 1]
+  end
+end
+
+perform
